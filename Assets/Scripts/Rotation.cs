ca3ed77fb5dc11328bc3a670096ad7a325531a17
yuanjea <47434686+yuanjea@users.noreply.github.com>
2019-03-25 16:05:50 -0600

    Interaction scripts

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,1,0), speed * Time.deltaTime, Space.World);
+    }
+}
---
+++ Assets/Scripts/SwipeRotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeRotate : MonoBehaviour
+{
+    float rotSpeed = 20;
+
+    public GameObject Earth;
+
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        float rotY = -1 * Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+        Earth.transform.Rotate(Vector3.up, -rotX, Space.World);
+        Earth.transform.Rotate(Vector3.right, rotY, Space.World);
+
+    }
+}
