e919eff16d7d11865b0ba6117de5c9f2ed73b2ea
jose-perez02 <rayzote@email.arizona.edu>
2019-04-11 20:43:58 -0700

    :bento: Added plane controller and dummy material
    The dummy material is intentionally included with transparent renderer
    mode so that final build expects transparent renders. This allows me to
    switch between modes during runtime; which is used for TrackOnFloor
    script.
    
    We can use the new plane controller static script so we can
    activate/deactivate plane manager and current detected planes in the
    real world.

---
+++ Assets/Scripts/Interaction/Miscellaneous/PlaneDetectionController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+/// <summary>
+/// This class demonstrates how to toggle plane detection,
+/// and also hide or show the existing planes.
+/// </summary>
+[RequireComponent(typeof(ARPlaneManager))]
+public class PlaneDetectionController : MonoBehaviour
+{
+    /// <summary>
+    /// Toggles plane detection and the visualization of the planes.
+    /// </summary>
+    public void TogglePlaneDetection()
+    {
+        m_ARPlaneManager.enabled = !m_ARPlaneManager.enabled;
+
+        if (m_ARPlaneManager.enabled)
+        {
+            SetAllPlanesActive(true);
+        }
+        else
+        {
+            SetAllPlanesActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Iterates over all the existing planes and activates
+    /// or deactivates their <c>GameObject</c>s'.
+    /// </summary>
+    /// <param name="value">Each planes' GameObject is SetActive with this value.</param>
+    public void SetAllPlanesActive(bool value)
+    {
+        m_ARPlaneManager.GetAllPlanes(s_Planes);
+        foreach (var plane in s_Planes)
+            plane.gameObject.SetActive(value);
+    }
+
+    void Awake()
+    {
+        m_ARPlaneManager = GetComponent<ARPlaneManager>();
+    }
+
+    ARPlaneManager m_ARPlaneManager;
+
+    static List<ARPlane> s_Planes = new List<ARPlane>();
+}
